6fac8cb5fdd29d526b7842e0d2397c4593b1866d
krolgongow <kamildk8@gmail.com>
2021-10-05 11:57:08 +0200

    master

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,198 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="cover off"></div>
+  <div class="result off">
+    <div class="winner">player1 won!</div>
+    <button class="newGame">New game</button>
+  </div>
+  <header>
+    <div class="player1 move"><p>Player 1 move</p></div>
+    <div class="player2 move inactive"><p>Player 2 move</p></div>
+  </header>
+  <main>
+    <div class="field" data-key="1"></div>
+    <div class="field" data-key="2"></div>
+    <div class="field" data-key="3"></div>
+    <div class="field" data-key="4"></div>
+    <div class="field" data-key="5"></div>
+    <div class="field" data-key="6"></div>
+    <div class="field" data-key="7"></div>
+    <div class="field" data-key="8"></div>
+    <div class="field" data-key="9"></div>
+  </main>
+  <button class="reset">reset</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url("https://fonts.googleapis.com/css2?family=Krona+One&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  width: 100vw;
+  font-family: "Krona One", sans-serif;
+}
+
+header {
+  position: fixed;
+  display: flex;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100px;
+  background-color: black;
+}
+
+.move {
+  width: 50%;
+  height: 100%;
+  line-height: 100px;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 40px;
+  text-transform: uppercase;
+  overflow: hidden;
+}
+
+.player1 {
+  align-self: flex-start;
+}
+
+.player2 {
+  align-self: flex-end;
+}
+
+.player1.inactive {
+  color: #333;
+}
+
+.player2.inactive {
+  color: #333;
+}
+
+main {
+  position: absolute;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-content: space-between;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 600px;
+  width: 600px;
+  background-color: black;
+}
+.field {
+  position: relative;
+  width: 190px;
+  height: 190px;
+  background-color: #fff;
+  cursor: pointer;
+  transition: 0.1s linear;
+}
+.field:hover {
+  background-color: #ddd;
+}
+
+.field img {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.reset {
+  border: 0;
+  height: 60px;
+  width: 180px;
+  font-family: "Krona One", sans-serif;
+  font-size: 20px;
+  background-color: #000;
+  cursor: pointer;
+  border-radius: 10px;
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  color: white;
+  transition: 0.1s linear;
+  text-transform: uppercase;
+}
+
+.reset:hover {
+  background-color: white;
+  color: black;
+  border: 2px solid black;
+}
+
+.cover {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  opacity: 0.5;
+  z-index: 1;
+}
+.cover.off {
+  display: none;
+}
+.result {
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 400px;
+  width: 600px;
+  border-radius: 25px;
+  background-color: black;
+  z-index: 2;
+}
+
+.result.off {
+  display: none;
+}
+
+.winner {
+  position: absolute;
+  top: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 200px;
+  width: 400px;
+  background-color: #fff;
+  border-radius: 25px;
+  text-align: center;
+  line-height: 200px;
+  font-size: 35px;
+  text-transform: uppercase;
+}
+
+.newGame {
+  border: 0;
+  position: absolute;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  text-transform: uppercase;
+  height: 60px;
+  width: 180px;
+  font-family: "Krona One", sans-serif;
+  font-size: 20px;
+  background-color: #fff;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.newGame:hover {
+  background-color: #ddd;
 }
 </style>
